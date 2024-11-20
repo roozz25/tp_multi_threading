8956b0cb036dc0ee06e4b3e1d6fdcf5e1399ba98
roozz25 <rahim.saidi98@gmail.com>
2024-11-20 15:54:55 +0100

    fin Tp 1

--- test_task.py
+++ test_task.py
@@ -4,11 +4,6 @@ from task import Task
 
 
 class TestWork(unittest.TestCase):
-    def test_task_equals_fail(self):
-        _task = Task()
-
-        np.testing.assert_almost_equal(_task.a @ _task.x, _task.b)
-
     def test_task_equals_pass(self):
         _task = Task()
         _task.work()
